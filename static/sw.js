0c4763a595ef5759febcd16b2d72f0f1f1e7df64
snehal593 <121617750+snehal593@users.noreply.github.com>
2026-06-04 13:24:52 +0530

    Delete static/sw.js
    old sw file

--- static/sw.js
+++
@@ -1,9 +0,0 @@
-// A simple Service Worker to satisfy PWA installation requirements
-self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installed');
-});
-
-self.addEventListener('fetch', (event) => {
-    // This allows the app to fetch data from the internet normally
-    event.respondWith(fetch(event.request));
-});
